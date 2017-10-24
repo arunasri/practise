ef0c92566af25a09ebc3e7f6c150bf2ab73eed66
Aruna Bolisetti <ruby.aruna28@gmail.com>
2017-10-24 13:53:46 -0400

    first commit

--- src/test/java/com/bank/tests/HomePageIT.java
+++ src/test/java/com/bank/tests/HomePageIT.java
@@ -13,8 +13,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import com.bank.pageobjects.EnrollPO;
-import com.bank.pageobjects.HomePagePO;
+import PageObjects.HomePagePO;
 
 public class HomePageIT extends BaseIT {
 	private WebDriver driver;
@@ -22,43 +21,47 @@ public class HomePageIT extends BaseIT {
 	private StringBuffer verificationErrors = new StringBuffer();
 
 	public static final String USERNAME = "arunasri";
-	public static final String SELENIUM_GRID = "https://" + USERNAME + ":" + System.getenv("SAUCE_LAB_KEY") + "@ondemand.saucelabs.com:443/wd/hub";
+	public static final String SELENIUM_GRID = "https://" + USERNAME + ":" + System.getenv("SAUCE_LAB_KEY")
+			+ "@ondemand.saucelabs.com:443/wd/hub";
 
 	@BeforeClass(alwaysRun = true)
 	public void SetUp() throws Exception {
-	    if (System.getenv("SAUCE_LAB_KEY") == null) {
-            driver = new ChromeDriver();
-        } else {
+		if (System.getenv("SAUCE_LAB_KEY") == null) {
+			driver = new ChromeDriver();
+		} else {
 
-            driver = new RemoteWebDriver(new URL(SELENIUM_GRID), DesiredCapabilities.chrome());
-        }
+			driver = new RemoteWebDriver(new URL(SELENIUM_GRID), DesiredCapabilities.chrome());
+		}
 		baseUrl = "https://www.wellsfargo.com/";
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 	}
-	
+
 	@Test
 	public void testWellsFargo() throws Exception {
 		// preconditions
 		driver.get(baseUrl);
 		HomePagePO home = new HomePagePO(driver);
-		home.enrollLink.click();
-		
-		EnrollPO enroll = new EnrollPO(driver);
-		enroll.continueButton.click();
-		enroll.homeLink.click();		
-		enroll.exitButton.click();
-	    enroll.wellsFargologo.click();
-		System.out.println("Logo link is clicked");
-		this.screenshot(driver,"home", "home_link.png");
+		home.searchTextfield.sendKeys("jobs wells fargo");
+		home.searchButton.click();
+
+		// home.enrollLink.click();
+
+		// EnrollPO enroll = new EnrollPO(driver);
+		// enroll.continueButton.click();
+		// enroll.homeLink.click();
+		// enroll.exitButton.click();
+		// enroll.wellsFargologo.click();
+		// System.out.println("Logo link is clicked");
+		// this.screenshot(driver,"home", "home_link.png");
 	}
-	
-		@AfterMethod
-		@AfterClass(alwaysRun = true)
-		public void tearDown() throws Exception {
-			driver.quit();
-			String verificationErrorString = verificationErrors.toString();
-			if (!"".equals(verificationErrorString)) {
-				Assert.fail(verificationErrorString);
-			}
+
+	@AfterMethod
+	@AfterClass(alwaysRun = true)
+	public void tearDown() throws Exception {
+		driver.quit();
+		String verificationErrorString = verificationErrors.toString();
+		if (!"".equals(verificationErrorString)) {
+			Assert.fail(verificationErrorString);
 		}
+	}
 }
