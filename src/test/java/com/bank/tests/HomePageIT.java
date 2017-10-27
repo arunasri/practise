782c0727b0c3da23af567edd9d8748069331f593
arunasri <ruby.aruna28@gmail.com>
2017-10-27 06:41:14 -0400

    remove keys

--- src/test/java/com/bank/tests/HomePageIT.java
+++ src/test/java/com/bank/tests/HomePageIT.java
@@ -22,8 +22,7 @@ public class HomePageIT extends BaseIT {
 	private StringBuffer verificationErrors = new StringBuffer();
 
 	public static final String USERNAME = "arunasri";
-	public static final String ACCESS_KEY = "5eff05a3-a91a-41aa-90ff-0a54533fe9ba";
-	public static final String SELENIUM_GRID = "https://" + USERNAME + ":" + ACCESS_KEY + "@ondemand.saucelabs.com:443/wd/hub";
+	public static final String SELENIUM_GRID = "https://" + USERNAME + ":" + System.getenv("SAUCE_LAB_KEY") + "@ondemand.saucelabs.com:443/wd/hub";
 
 	@BeforeClass(alwaysRun = true)
 	public void SetUp() throws Exception {
